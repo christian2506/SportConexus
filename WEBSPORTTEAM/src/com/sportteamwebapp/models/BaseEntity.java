0055938e0402d2882f8b90fc45ec687c70717431
christian2506 <1410884@utp.edu.pe>
2017-02-27 10:38:51 -0500

    Pro

---
+++ WEBSPORTTEAM/src/com/sportteamwebapp/models/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.sportteamwebapp.models;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by GrupoUTP on 01/10/2016.
+ */
+public class BaseEntity {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    protected int executeUpdate(String sql) {
+        try {
+            int result = getConnection()
+                    .createStatement()
+                    .executeUpdate(sql);
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
+
+
